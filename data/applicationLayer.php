a9bf543693035917d1fe2ae62f544c000a77d153
Marco <MarcoRmz@users.noreply.github.com>
2017-02-21 15:47:45 -0600

    added communication layers

---
+++ data/applicationLayer.php
@@ -0,0 +1,153 @@
+<?php
+	header('Content-type: application/json');
+	require_once __DIR__ . '/dataLayer.php';
+
+	$action = $_POST["action"];
+
+	switch ($action) {
+		case 'LOGIN':
+			loginFunction();
+			break;
+		case 'REGISTRATION':
+			registrationFunction();
+			break;
+		case 'LOGOUT':
+			logoutFunction();
+			break;
+		case 'CHECKSESSION':
+			sessionCheck();
+			break;
+		case 'CHECKCOOKIE':
+			cookieCheck();
+			break;
+	}
+
+	function sessionCheck() {
+		session_start();
+
+		if (isset($_SESSION['user'])) {
+			$msg = 'Logout ' . $_SESSION['user'];
+			echo json_encode(array("loginMSG" => $msg));
+		} else {
+			header('HTTP/1.1 500 No Session');
+			die('No Session');
+		}
+	}
+
+	function cookieCheck() {
+		if (isset($_COOKIE['userEmail'])) {
+			echo json_encode(array("cookie" => $_COOKIE['userEmail']));
+		} else {
+			header('HTTP/1.1 500 No Cookie');
+			die('No cookie');
+		}
+	}
+
+	function logoutFunction() {
+		session_start();
+
+		if (isset($_SESSION['user'])) {
+			session_destroy();
+			echo json_encode(array("message" => 'Session removed'));
+		} else {
+			header('HTTP/1.1 500 No Session');
+			die('No Session');
+		}
+	}
+
+	function passwordDecryption($password) {
+		$key = pack('H*', "dbf14c7e313a05bfa34763051cef08bc55abe029fdebae5e1d417e2ffb2c11a3");
+
+		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+
+		$ciphertext_dec = base64_decode($password);
+		$iv_dec = substr($ciphertext_dec, 0, $iv_size);
+		$ciphertext_dec = substr($ciphertext_dec, $iv_size);
+
+		$password = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+			
+			$count = 0;
+			$length = strlen($password);
+
+		for ($i = $length - 1; $i >= 0; $i --) {
+			if (ord($password{$i}) === 0) {
+				$count ++;
+			}
+		}
+
+		$password = substr($password, 0,  $length - $count); 
+
+		return $password;
+	}
+
+	function passwordEncryption($password) {
+	    $key = pack('H*', "dbf14c7e313a05bfa34763051cef08bc55abe029fdebae5e1d417e2ffb2c11a3");
+
+	    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+	    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	    
+	    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $password, MCRYPT_MODE_CBC, $iv);
+	    $ciphertext = $iv . $ciphertext;
+	    
+	    $userPassword = base64_encode($ciphertext);
+
+	    return $userPassword;
+	}
+
+	function loginFunction() {
+		$userEmail = $_POST["email"];
+		$userPassword = $_POST["password"];
+		$cookie = $_POST["cookie"];
+
+		$result = findUser($userEmail);
+
+		if ($result["status"] == "SUCCESS") {
+			// Decrypt password
+			$pass = passwordDecryption($result["response"]);
+
+			if ($userPassword == $pass) {
+				session_start();
+				$_SESSION["user"] = $userEmail;
+
+				if($cookie == "true"){
+					if($_COOKIE["userEmail"] != username){
+						setcookie("userEmail", $userEmail, time()+(60*60*24*20), "/", "", 0);
+					}
+				} else {
+					setcookie("userEmail", '', time()-(60*60*24*30), "/", "", 0);
+				}
+				echo json_encode(array("message" => 'Login succesful!'));
+			} else {
+				header('HTTP/1.1 500 ' . $result["status"]);
+				die($result["status"]);
+			}
+		} else {
+			header('HTTP/1.1 500 ' . $result["status"]);
+			die($result["status"]);
+		}
+	}
+
+	function registrationFunction() {
+		$fName = $_POST["firstName"];
+		$lName = $_POST["lastName"];
+		$userName = $_POST["username"];
+		$userEmail = $_POST["email"];
+		$userPassword = $_POST["password"];
+
+		$userPassword = passwordEncryption($userPassword);
+
+		$result = attemptRegistration($userEmail, $fName, $lName, $userName, $userPassword);
+
+		if ($result["status"] == "SUCCESS") {
+			session_start();
+			$_SESSION["user"] = $userEmail;
+
+			setcookie("userEmail", '', time()-(60*60*24*30), "/", "", 0);
+
+			echo json_encode(array("message" => "Registration succesful!"));
+		} else {
+			header('HTTP/1.1 500 ' . $result["status"]);
+			die($result["status"]);
+		}
+	}
+?>
---
+++ data/dataLayer.php
@@ -0,0 +1,64 @@
+<?php
+
+	function connectionToDataBase() {
+		$servername = "localhost";
+		$username = "root";
+		$password = "root";
+		$dbname = "start";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+		if ($conn->connect_error) {
+			return null;
+		} else {
+			return $conn;
+		}
+	}
+
+	function findUser($userEmail) {
+		$conn = connectionToDataBase();
+
+		if ($conn != null) {
+			$sql = "SELECT * FROM Users WHERE email='$userEmail'";
+
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					$conn->close();
+					return array("status" => "SUCCESS", "response" => $row['passwrd']);
+				}
+			} else {
+				return array("status" => "User not Found!");
+			}
+		} else {
+			return array("status" => "Connection with DB went wrong!");
+		}
+		$conn -> close();
+	}
+
+	function attemptRegistration($userEmail, $fName, $lName, $userName, $userPassword) {
+		$conn = connectionToDataBase();
+
+		if ($conn != null) {
+			$sql = "SELECT email FROM Users WHERE email='$userEmail'";
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+				return array("status" => "Email already exists!");
+			} else {
+				$sql = "INSERT INTO Users(fName, lName, username, email, passwrd) VALUES('$fName', '$lName', '$userName', '$userEmail', '$userPassword');";
+
+				if (mysqli_query($conn, $sql)) {
+					return array("status" => "SUCCESS");
+				} 
+				else {
+				    return array("status" => "Couldn't create user!");
+				}
+			}
+		} else {
+			return array("status" => "Connection with DB went wrong!");
+		}
+		$conn -> close();
+	}
+?>
