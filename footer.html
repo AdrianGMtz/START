1a4f3e037e570813d30cbc8b27f142d17373c358
a01280252 <agmtzq@gmail.com>
2017-02-25 18:13:56 -0600

    Separate files for header, footer and navbar
    Added separate html and php files for elements that will repeat on every
    page. Made separate file for login javascript.

---
+++ footer.html
@@ -0,0 +1,8 @@
+<footer class="page-footer black">
+	<div class="footer-copyright">
+		<div class="container">
+			© 2017 START. All rights reserved
+			<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+		</div>
+	</div>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,9 @@
+<nav>
+	<div class="nav-wrapper black">
+		<a href="index.html" class="brand-logo"> START! </a>
+		<ul id="nav-mobile" class="right"> <!-- The navigation links are aligned to the right -->	
+			<li><a href="signup.html"> Sign Up </a></li>
+			<li><a href="login.html"> Login </a></li>
+		</ul>
+	</div>
+</nav>
