8bb5fdc68bd3ca99956e5751a27d77968b5dba19
AngelGonzalez <ange.gonzalezg@hotmail.com>
2017-03-21 09:28:44 -0600

    Change Password
    Added settings.php and a settings button in the navbar.

--- navbar.php
+++ navbar.php
@@ -4,6 +4,8 @@
 		<ul id="nav-mobile" class="right"> <!-- The navigation links are aligned to the right -->
 			<div class="loggedIN">
 				<li><a href="profile.php" id="userID"></a></li>
+				<li><a href="settings.php"><i class="material-icons left">settings</i>
+				</a></li>
 				<li><a href="inbox.php"><i class="material-icons left">email</i><span class="new badge red" id="unreadMSG"> 2 </span></a></li>
 				<li id="logout" style="padding-right: 15px; padding-left: 15px;">Logout</li>
 			</div>
