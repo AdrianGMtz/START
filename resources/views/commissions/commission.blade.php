6dd1e48fcbc3686eb45afef335a0c2e31bf6cd0a
Marco <MarcoRmz@users.noreply.github.com>
2017-03-28 18:41:51 -0600

    Added edit button in commissions in profile

--- resources/views/commissions/commission.blade.php
+++ resources/views/commissions/commission.blade.php
@@ -1,8 +1,11 @@
 <div class="col s3">
-	<div class="card small">
+	<div class="card">
 		<div class="card-image">
 			<img src="https://udemy-images.udemy.com/course/750x422/394968_538b_7.jpg">
 			{{-- <span class="card-title black-text">{{ $commission->user->name }}</span> --}}
+			@if ($user->id == auth()->user()->id)
+				<a class="btn-floating halfway-fab waves-effect waves-light blue" href="/commissions/{{ $commission->id }}/edit"><i class="material-icons">mode_edit</i></a>
+			@endif
 		</div>
 		<div class="card-content">
 			{{-- <p>{{ $commission->user->name }}</p> --}}
@@ -12,4 +15,6 @@
 			<a href="commissions/{{ $commission->id }}">More Info</a>
 		</div>
 	</div>
+
 </div>
+
