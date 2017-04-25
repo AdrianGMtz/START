234d7f2b90b418adbe17b9b05805d05beb1dd75f
Marco <MarcoRmz@users.noreply.github.com>
2017-04-24 21:49:36 -0500

    Added chat and order buttons to nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,5 +43,10 @@
 	<!-- Scripts -->
 	<script src="{{ asset('js/app.js') }}"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$(".dropdown-button").dropdown({hover:true});
+		});
+	</script>
 </body>
 </html>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,46 +1,63 @@
 <nav>
-    <div class="nav-wrapper black">
-        <!-- Branding Image -->
-        <a class="brand-logo" href="{{ url('/') }}">
-            {{ config('app.name') }}
-        </a>
-        <ul id="nav-mobile" class="right"> <!-- The navigation links are aligned to the right -->
-            <!-- Authentication Links -->
-            @if (Auth::guest())
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Register</a></li>
-            @else
-                <li><a href="/profile">{{ Auth::user()->name }}</a></li>
-                <li>
-                    <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                        Logout
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-                {{-- <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        {{ Auth::user()->name }}
-                    </a>
-
-                    <ul class="dropdown-menu" role="menu">
-                        <li>
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                                Logout
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </li>
-                    </ul>
-                </li> --}}
-            @endif
-        </ul>
-    </div>
+	<div class="nav-wrapper black">
+		<!-- Branding Image -->
+		<a class="brand-logo" href="{{ url('/') }}">
+			{{ config('app.name') }}
+		</a>
+		<ul id="nav-mobile" class="right"> <!-- The navigation links are aligned to the right -->
+			<!-- Authentication Links -->
+			@if (Auth::guest())
+				<li><a href="{{ route('login') }}">Login</a></li>
+				<li><a href="{{ route('register') }}">Register</a></li>
+			@else
+				<li><a href="/profile">{{ Auth::user()->name }}</a></li>
+				<li><a href="/messages">Chat <span class="material-icons">forum</span></a></li>
+				<li><a href="/orders">Orders <span class="material-icons">description</span></a></li>
+				
+				<li>
+					<a href="{{ route('logout') }}"
+						onclick="event.preventDefault();
+								 document.getElementById('logout-form').submit();">
+						Logout
+					</a>
+					<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+						{{ csrf_field() }}
+					</form>
+				</li>
+			@endif
+		</ul>
+	</div>
 </nav>
+
+{{-- Dropdown menu --}}
+{{-- <ul id="menu_dropdown" class="dropdown-content">
+	<li><a href="#!">Settings</a></li>
+	<li>
+		<a href="{{ route('logout') }}"
+			onclick="event.preventDefault();
+					 document.getElementById('logout-form').submit();">
+			Logout
+		</a>
+		<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+			{{ csrf_field() }}
+		</form>
+	</li>
+</ul>
+<nav>
+	<div class="nav-wrapper black">
+		<!-- Branding Image -->
+		<a class="brand-logo" href="{{ url('/') }}">
+			{{ config('app.name') }}
+		</a>
+		<ul id="nav-mobile" class="right"> <!-- The navigation links are aligned to the right -->
+			<!-- Authentication Links -->
+			@if (Auth::guest())
+				<li><a href="{{ route('login') }}">Login</a></li>
+				<li><a href="{{ route('register') }}">Register</a></li>
+			@else
+				<li><a href="/profile">{{ Auth::user()->name }}</a></li>
+				<li><a class="dropdown-button" href="#!" data-activates="menu_dropdown"><i class="material-icons right">arrow_drop_down</i></a></li>
+			@endif
+		</ul>
+	</div>
+</nav> --}}
